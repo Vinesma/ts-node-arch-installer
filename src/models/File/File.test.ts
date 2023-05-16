b74030ead63bc3ccc70f11f5fd1da06851e82493
Otavio Cornelio <otaviocos14@gmail.com>
2023-05-15 23:40:08 -0300

    test: update File tests to mock mkdir

--- src/models/File/File.test.ts
+++ src/models/File/File.test.ts
@@ -1,12 +1,61 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
+import fs from "node:fs";
 import os from "node:os";
 import File from "./File";
+import { beforeEach } from "node:test";
+
+jest.mock("node:fs", () => ({
+    mkdir: jest.fn(path => path),
+}));
+
+const mkdirMock = jest.mocked(fs.mkdir);
+
+const HOME = os.homedir();
+const testFile = new File("test.txt", "~/Projects");
 
 describe("A file", () => {
-    it("should handle '~' in filepaths", () => {
-        const file = new File("test.txt", "~/Projects");
-        const expectedPath = `${os.homedir()}/Projects`;
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
-        expect(file.destination_path).toBe(expectedPath);
+    it("should handle '~' in filepaths", () => {
+        const expectedPath = `${HOME}/Projects`;
+
+        expect(testFile.destination_path).toBe(expectedPath);
+    });
+
+    it("should handle absolute paths", () => {
+        const expectedPath = `${HOME}/Projects/test.txt`;
+
+        expect(testFile.absolute_path).toBe(expectedPath);
+    });
+
+    it("should make a directory at the proper path", () => {
+        expect(jest.isMockFunction(fs.mkdir)).toBeTruthy();
+
+        testFile.mkdir();
+
+        expect(fs.mkdir).toBeCalledTimes(1);
+        expect(fs.mkdir).toReturnWith(testFile.destination_path);
+    });
+
+    it("should error on fail", () => {
+        expect(jest.isMockFunction(fs.mkdir)).toBeTruthy();
+        expect(jest.isMockFunction(mkdirMock)).toBeTruthy();
+
+        const mkdirMockImplementation = {
+            __promisify__: async () => {
+                throw Error;
+            },
+        };
+
+        mkdirMock.mockImplementation(
+            mkdirMockImplementation as unknown as typeof fs.mkdir
+        );
+
+        testFile.mkdir();
+
+        expect(mkdirMock).toBeCalledTimes(1);
+        expect(mkdirMock).toThrow();
     });
 });
